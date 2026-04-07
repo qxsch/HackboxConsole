128ddafeb332d6e9471524818c90266f420f284e
Marco Weber <marcoweber@microsoft.com>
2026-04-07 19:20:51 +0200

    added documentation ignore

--- iac/deployHackerConsole.ps1
+++ iac/deployHackerConsole.ps1
@@ -303,7 +303,7 @@ $zipPackagePath = Join-Path $scriptPath "hack_console.zip"
 if(Test-Path $zipPackagePath) {
     Remove-Item $zipPackagePath -Force
 }
-Get-ChildItem $consoleRoot | Where-Object { $_.Name -notin @( "iac", "createdEntraIdUserSettings.json" ) -and (-not $_.Name.EndsWith('.csv')) } | Compress-Archive -DestinationPath $zipPackagePath
+Get-ChildItem $consoleRoot | Where-Object { $_.Name -notin @( "iac", "createdEntraIdUserSettings.json", "documentation" ) -and (-not $_.Name.EndsWith('.csv')) } | Compress-Archive -DestinationPath $zipPackagePath
 Write-Host "Publishing the zip package to the web app"
 Publish-AzWebApp -ResourceGroupName $ResourceGroupName -Name $deployment.Outputs.webAppName.Value -ArchivePath $zipPackagePath -Force -ErrorAction Stop | Out-Null
 
